2da1cfdbfe030ce112e2dceaca1e8e6fbc8e4d20
SoftVibes <chickenpakale@gmail.com>
2022-04-19 18:24:48 +0530

    Finished everything yay!

--- project_setup.sh
+++ project_setup.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ]
 then
-    echo "Missing operation parameter. Type $0 help for details."
+    echo "Missing operation parameter. Type [$0 help] for details."
 
 elif [ $1 == "help" ] || [ $1 == "--help" ] || [ $1 == "/?" ]
 then
@@ -47,5 +47,5 @@ then
     echo "alias run=\"run\"" >> .bash_aliases
 
 else
-    echo "Invalid parameter [$1]."
+    echo "Invalid parameter [$1]. Type [$0 help] for details."
 fi
--- tools.sh
+++ tools.sh
@@ -1,47 +1,94 @@
 #!/bin/bash
 
-echo "Installing python ..."
-echo ""
-echo "sudo apt install python3"
-sudo apt install python3
-echo ""
-echo "sudo apt install python3-pip"
-sudo apt install python3-pip
-cd /home/$USER/
-if [ -f ".bash_aliases" ]
+if [ -z $1 ]
 then
-    echo "alias python=\"python3\"" >> .bash_aliases
-    echo "alias pip=\"pip3\"" >> .bash_aliases
+    echo "Missing operation parameter. Type [$0 help] for details."
 
-else
-    touch .bash_aliases
-    echo "alias python=\"python3\"" >> .bash_aliases
-    echo "alias pip=\"pip3\"" >> .bash_aliases
-fi
-echo ""
-echo ""
+elif [ $1 == "install" ]
+then
+    echo "Installing python ..."
+    echo ""
+    echo "sudo apt install python3"
+    sudo apt install python3
+    echo ""
+    echo "sudo apt install python3-pip"
+    sudo apt install python3-pip
+    cd /home/$USER/
+    if [ -f ".bash_aliases" ]
+    then
+        echo "alias python=\"python3\"" >> .bash_aliases
+        echo "alias pip=\"pip3\"" >> .bash_aliases
 
-echo "Installing NodeJS ..."
-echo ""
-echo "sudo apt install nodejs"
-sudo apt install nodejs
-echo "sudo apt install npm"
-sudo apt install npm
-echo ""
-echo ""
+    else
+        touch .bash_aliases
+        echo "alias python=\"python3\"" >> .bash_aliases
+        echo "alias pip=\"pip3\"" >> .bash_aliases
+    fi
+    echo ""
+    echo ""
 
-echo "Installing GNU C++ ..."
-echo ""
-echo "sudo apt install build-essential"
-sudo apt install build-essential
-echo ""
-echo ""
+    echo "Installing NodeJS ..."
+    echo ""
+    echo "sudo apt install nodejs"
+    sudo apt install nodejs
+    echo "sudo apt install npm"
+    sudo apt install npm
+    echo ""
+    echo ""
 
-echo "Installing Visual Studio Code ..."
-echo ""
-echo "sudo apt install code"
-sudo apt install code
-echo ""
-echo ""
+    echo "Installing GNU C++ ..."
+    echo ""
+    echo "sudo apt install build-essential"
+    sudo apt install build-essential
+    echo ""
+    echo ""
 
-echo "Installed Python, NodeJS, NPM, GNU C++ & Visual Studio Code."
+    echo "Installing Visual Studio Code ..."
+    echo ""
+    echo "sudo apt install code"
+    sudo apt install code
+    echo ""
+    echo ""
+
+    echo "Installed Python, NodeJS, NPM, GNU C++ & Visual Studio Code."
+
+elif [ $1 == "uninstall" ]
+then
+    echo "Uninstalling python ..."
+    echo ""
+    echo "sudo apt remove python3"
+    sudo apt remove python3
+    echo ""
+    echo "sudo apt remove python3-pip"
+    sudo apt remove python3-pip
+    cd /home/$USER/
+    if [ -f ".bash_aliases" ]
+    then
+        echo "alias python=\"python\"" >> .bash_aliases
+        echo "alias pip=\"pip\"" >> .bash_aliases
+    fi
+    echo ""
+    echo ""
+
+    echo "Uninstalling NodeJS ..."
+    echo ""
+    echo "sudo apt remove nodejs"
+    sudo apt remove nodejs
+    echo "sudo apt remove npm"
+    sudo apt remove npm
+    echo ""
+    echo ""
+
+    echo "Uninstalling GNU C++ ..."
+    echo ""
+    echo "sudo apt remove build-essential"
+    sudo apt remove build-essential
+    echo ""
+    echo ""
+
+    echo "Uninstalling Visual Studio Code ..."
+    echo ""
+    echo "sudo apt remove code"
+    sudo apt remove code
+    echo ""
+    echo ""
